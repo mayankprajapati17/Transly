1da56d703293266e56966f32ac2a0a38a531edf4
Mayank Prajapati <mayankprajapati2703@gmail.com>
2024-11-14 11:16:31 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,6 +66,7 @@ function App() {
             style={styles.select}
           >
             <option value="es">Spanish</option>
+            <option value="en">English</option>
             <option value="fr">French</option>
             <option value="de">German</option>
             <option value="zh-CN">Chinese</option>
